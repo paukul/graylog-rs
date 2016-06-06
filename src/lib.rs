e488588f2f84a28045e3bf3c975bee6d90704838
Pascal Friederich <paukul@gmail.com>
2016-06-06 12:45:42 +0200

    first module level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,19 @@
+//! The graylog crate provides graylog (https://www.graylog.org/) compatible logging based on
+//! the log crate and its logging facade (https://doc.rust-lang.org/log/log/index.html).
+//!
+//! # Examples
+//!
+//! ```
+//! #[macro_use] extern crate log;
+//! extern crate graylog;
+//!
+//! fn main() {
+//!   graylog::logger::init();
+//!   info!("The logger macros now use the graylog logging backend");
+//! }
+//! ```
+
+
 // Needed to derive `Serialize` on ServiceProperties
 #![feature(custom_derive, plugin)]
 #![plugin(serde_macros)]
